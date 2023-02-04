7117c3b4509504503289ebfbf97282de352ee8a4
Dani <dani@localhost>
2023-02-04 16:01:16 +0100

    saturday work

---
+++ Naami.Distributor.Data/Migrations/20230204124309_init.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Naami.Distributor.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShareTypes",
+                columns: table => new
+                {
+                    ObjectType = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Symbol = table.Column<string>(type: "text", nullable: false),
+                    TotalSupply = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    RegistryObjectId = table.Column<string>(type: "text", nullable: false),
+                    MetadataObjectId = table.Column<string>(type: "text", nullable: false),
+                    TxDigest = table.Column<string>(type: "text", nullable: true),
+                    EventSeq = table.Column<decimal>(type: "numeric(20,0)", nullable: true),
+                    NextTxDigest = table.Column<string>(type: "text", nullable: true),
+                    NextEventSeq = table.Column<decimal>(type: "numeric(20,0)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShareTypes", x => x.ObjectType);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShareTypes");
+        }
+    }
+}
